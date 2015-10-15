5f4cf8514d142460ce3f91cd21421f1dd67c965d
Cordooba <cordooba_92@hotmail.es>
2015-10-15 15:25:20 +0200

    Se ha implementado el registro de usuario, aun por terminar

--- register.html.php
+++ register.html.php
@@ -8,6 +8,38 @@
 </head>
 <body>
 	<div class="container">
+		<header>
+			<h1>Registro de usuario.</h1>
+			<hr>
+		</header>
+		<form action="" method="POST">
+		<div class="form-group">
+			<label for="name">Nombre:</label>
+		</div>	
+		<div class="form-group">
+			<label for="user">Usuario:</label>
+		</div>
+		<div class="form-group">	
+			<label for="psw">Contraseña:</label>
+		</div>
+		<div class="form-group">	
+			<label for="psw2">Repetir contraseña:</label>
+		</div>
+		<div class="form-group">	
+			<label for="email">E-mail:</label>
+		</div>
+		<div class="form-group">	
+			<label for="photo">Foto del usuario:</label>
+		</div>
+		<div class="form-group">	
+			<textarea name="description" id="description" cols="40" rows="15" placeholder="Breve descripción..."></textarea>
+		</div>
+		<div>
+			<button class="btn btn-primary">Registrar</button>
+		</div>
+		</form>
+	</div>
+	<div id="a" class="container">
 		<a href="login.html.php">Volver atrás.</a>
 	</div>
 </body>
